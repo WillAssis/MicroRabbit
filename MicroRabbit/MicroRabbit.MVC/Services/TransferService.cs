855e749c99fd0ab36e55a4dc56ce79fc74128cbc
Willian <willian_1802@hotmail.com>
2020-07-03 02:06:16 -0300

    Initial MVC Project to create a Transfer

---
+++ MicroRabbit/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,30 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            this.apiClient = apiClient;
+        }
+
+        public async Task Transfer(TransferDTO transferDTO)
+        {
+            var uri = "http://localhost:60114/api/Banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDTO),
+                                            System.Text.Encoding.UTF8, "application/json");
+            
+            var response = await apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
